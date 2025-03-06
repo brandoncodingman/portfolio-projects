f1e19b35a5e60219c4d2b5dd92b4516ce49acf07
brandoncodingman <brandoncodingman@gmail.com>
2025-03-06 14:18:53 +0900

    smash added

---
+++ Smash/script.js
@@ -0,0 +1,9 @@
+const loader = document.getElementById('loading-screen');
+
+document.addEventListener('keydown', () => {
+    loader.style.display = "none";
+});
+
+document.addEventListener('click', () => {
+    loader.style.display = "none";
+});
