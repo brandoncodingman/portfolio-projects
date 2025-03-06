52722e4eeb97234f2fdf319d7c4f26c238e79fe5
brandoncodingman <brandoncodingman@gmail.com>
2025-03-06 10:30:56 +0900

    zoo update

---
+++ kanazawazooproject/JS/navbar.js
@@ -0,0 +1,6 @@
+// Toggle Navbar visibility on smaller screens
+document.getElementById('navbarToggle').addEventListener('click', function() {
+    const navbarContent = document.getElementById('navbarContent');
+    navbarContent.classList.toggle('active');
+  });
+  
