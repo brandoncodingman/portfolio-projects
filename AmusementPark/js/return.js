e2617a9049ef8c59bc29cceba9af493ec3430d23
brandoncodingman <brandoncodingman@gmail.com>
2025-03-18 15:41:01 +0900

    fixed return function

--- AmusementPark/js/return.js
+++ AmusementPark/js/return.js
@@ -1,7 +1,7 @@
 
     document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("home").addEventListener("click", function () {
-            const navbar = document.querySelector(".nav-header");
+            const navbar = document.querySelector(".tag");
             if (navbar) {
                 navbar.scrollIntoView({ behavior: "smooth" });
             } else {
